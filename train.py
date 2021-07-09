dc6d1e78c99ad4a8af0911dd780b32e7584b1e5f
Zishen Chen <32064967+jasonchan117@users.noreply.github.com>
2021-07-10 05:08:01 +0800

    Modify to multi-frames

--- train.py
+++ train.py
@@ -16,7 +16,9 @@ from torch.autograd import Variable
 from dataset.dataset_nocs import Dataset
 from libs.network import KeyNet
 from libs.loss import Loss
-
+from matplotlib import pyplot as plt
+import sys
+from tqdm import tqdm
 cate_list = ['bottle', 'bowl', 'camera', 'can', 'laptop', 'mug']
 
 parser = argparse.ArgumentParser()
@@ -29,48 +31,82 @@ parser.add_argument('--workers', type=int, default = 5, help='number of data loa
 parser.add_argument('--num_kp', type=int, default = 8, help='number of kp')
 parser.add_argument('--outf', type=str, default = 'models/', help='save dir')
 parser.add_argument('--lr', default=0.0001, help='learning rate')
+parser.add_argument('--cuda', action='store_true')
+parser.add_argument('--w_size', default=5, type=int)
 opt = parser.parse_args()
 
-model = KeyNet(num_points = opt.num_points, num_key = opt.num_kp, num_cates = opt.num_cates)
-model.cuda()
+model = KeyNet(num_points = opt.num_points, num_key = opt.num_kp, num_cates = opt.num_cates, opt = opt)
+if opt.cuda == True:
+    model.cuda()
 
 if opt.resume != '':
     model.load_state_dict(torch.load('{0}/{1}'.format(opt.outf, opt.resume)))
-
-dataset = Dataset('train', opt.dataset_root, True, opt.num_points, opt.num_cates, 5000, opt.category)
+# The num_points is a predefined value which is 500 and it means the maximum 3-d points needed to be considered for one object. This value is equal to the number of points in point cloud and choose list.
+dataset = Dataset('train', opt.dataset_root, True, opt.num_points, opt.num_cates, 5000, opt.category, opt.w_size)
 dataloader = torch.utils.data.DataLoader(dataset, batch_size=1, shuffle=True, num_workers=opt.workers)
-test_dataset = Dataset('val', opt.dataset_root, False, opt.num_points, opt.num_cates, 1000, opt.category)
+test_dataset = Dataset('val', opt.dataset_root, False, opt.num_points, opt.num_cates, 1000, opt.category, opt.w_size)
 testdataloader = torch.utils.data.DataLoader(test_dataset, batch_size=1, shuffle=True, num_workers=opt.workers)
 
-criterion = Loss(opt.num_kp, opt.num_cates)
+criterion = Loss(opt.num_kp, opt.num_cates, opt=opt)
 
 best_test = np.Inf
 optimizer = optim.Adam(model.parameters(), lr=opt.lr)
 
-for epoch in range(0, 500):
+for epoch in tqdm(range(0, 500)):
     model.train()
     train_dis_avg = 0.0
     train_count = 0
 
     optimizer.zero_grad()
 
-    for i, data in enumerate(dataloader, 0):
+    for i, data in tqdm(enumerate(dataloader, 0)):
         img_fr, choose_fr, cloud_fr, r_fr, t_fr, img_to, choose_to, cloud_to, r_to, t_to, mesh, anchor, scale, cate = data
-        img_fr, choose_fr, cloud_fr, r_fr, t_fr, img_to, choose_to, cloud_to, r_to, t_to, mesh, anchor, scale, cate = Variable(img_fr).cuda(), \
-                                                                                                                     Variable(choose_fr).cuda(), \
-                                                                                                                     Variable(cloud_fr).cuda(), \
-                                                                                                                     Variable(r_fr).cuda(), \
-                                                                                                                     Variable(t_fr).cuda(), \
-                                                                                                                     Variable(img_to).cuda(), \
-                                                                                                                     Variable(choose_to).cuda(), \
-                                                                                                                     Variable(cloud_to).cuda(), \
-                                                                                                                     Variable(r_to).cuda(), \
-                                                                                                                     Variable(t_to).cuda(), \
-                                                                                                                     Variable(mesh).cuda(), \
-                                                                                                                     Variable(anchor).cuda(), \
-                                                                                                                     Variable(scale).cuda(), \
-                                                                                                                     Variable(cate).cuda()
+        '''
+        Output size: img_fr:torch.Size([1, 3, 320, 160])||choose_fr:torch.Size([1, 1, 500])||cloud_fr:torch.Size([1, 500, 3])||r_fr:torch.Size([1, 3, 3])||t_fr:torch.Size([1, 3])||mesh:torch.Size([1, 3895, 3])||anchor:torch.Size([1, 125, 3])
+        img_fr: The image after 2d bounding box cropping, while the original size of the image is 480 x 640.
+        Choose: 1-d list
+        Cloud: 2-d matrix indicate the 3d coordinates of the object.
+        anchor: The 3-d coordinates of generated anchors.
+        cate: The ground truth category.
+        '''
+        # plt.imshow(torch.transpose(img_fr.squeeze(0),0,2).numpy() * 255.)
+        # plt.show()
+        # plt.imshow(torch.transpose(img_to.squeeze(0),0,2).numpy() * 255.)
+        # plt.show()
+        # print('img_fr:{}||choose_fr:{}||cloud_fr:{}||r_fr:{}||t_fr:{}||mesh:{}||anchor:{}||scale:{}||cate:{}'.format(img_fr.size(),choose_fr.size(),cloud_fr.size(),r_fr.size(),t_fr.size(),mesh.size(),anchor.size(),scale.size(),cate.size()))
 
+        # sys.exit()
+        if opt.cuda == True:
+            img_fr, choose_fr, cloud_fr, r_fr, t_fr, img_to, choose_to, cloud_to, r_to, t_to, mesh, anchor, scale, cate = Variable(img_fr).cuda(), \
+                                                                                                                         Variable(choose_fr).cuda(), \
+                                                                                                                         Variable(cloud_fr).cuda(), \
+                                                                                                                         Variable(r_fr).cuda(), \
+                                                                                                                         Variable(t_fr).cuda(), \
+                                                                                                                         Variable(img_to).cuda(), \
+                                                                                                                         Variable(choose_to).cuda(), \
+                                                                                                                         Variable(cloud_to).cuda(), \
+                                                                                                                         Variable(r_to).cuda(), \
+                                                                                                                         Variable(t_to).cuda(), \
+                                                                                                                         Variable(mesh).cuda(), \
+                                                                                                                         Variable(anchor).cuda(), \
+                                                                                                                         Variable(scale).cuda(), \
+                                                                                                                         Variable(cate).cuda()
+        else:
+            img_fr, choose_fr, cloud_fr, r_fr, t_fr, img_to, choose_to, cloud_to, r_to, t_to, mesh, anchor, scale, cate = Variable(img_fr), \
+                                                                                                                         Variable(choose_fr), \
+                                                                                                                         Variable(cloud_fr), \
+                                                                                                                         Variable(r_fr), \
+                                                                                                                         Variable(t_fr), \
+                                                                                                                         Variable(img_to), \
+                                                                                                                         Variable(choose_to), \
+                                                                                                                         Variable(cloud_to), \
+                                                                                                                         Variable(r_to), \
+                                                                                                                         Variable(t_to), \
+                                                                                                                         Variable(mesh), \
+                                                                                                                         Variable(anchor), \
+                                                                                                                         Variable(scale), \
+                                                                                                                         Variable(cate)
+        # kp_fr: (1, 8, 3), anc_fr:(1, 125, 3), att_fr:(1, 125)
         Kp_fr, anc_fr, att_fr = model(img_fr, choose_fr, cloud_fr, anchor, scale, cate, t_fr)
         Kp_to, anc_to, att_to = model(img_to, choose_to, cloud_to, anchor, scale, cate, t_to)
 
