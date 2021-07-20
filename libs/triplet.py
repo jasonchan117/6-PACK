b682c3cd1643a4124d161c5bd885cbb31cdeb416
Zishen Chen <32064967+jasonchan117@users.noreply.github.com>
2021-07-21 05:50:08 +0800

    Add codes.

---
+++ libs/triplet.py
@@ -0,0 +1,105 @@
+import torch.nn as nn
+import torch.nn.functional as F
+import torch
+
+class EmbeddingNet(nn.Module):
+    def __init__(self):
+        super(EmbeddingNet, self).__init__()
+        self.convnet = nn.Sequential(nn.Conv2d(3, 32, 5), nn.PReLU(),
+                                     nn.MaxPool2d(2, stride=2),
+                                     nn.Conv2d(32, 64, 5), nn.PReLU(),
+                                     nn.MaxPool2d(2, stride=2))
+
+        self.fc = nn.Sequential(nn.Linear( 576, 256),
+                                nn.PReLU(),
+                                nn.Linear(256, 256),
+                                nn.PReLU(),
+                                nn.Linear(256, 128)
+                                )
+
+    def forward(self, x):
+        output = self.convnet(x)
+        output = output.view(output.size()[0], -1)
+        output = self.fc(output)
+        return output
+
+    def get_embedding(self, x):
+        return self.forward(x)
+
+
+class EmbeddingNetL2(EmbeddingNet):
+    def __init__(self):
+        super(EmbeddingNetL2, self).__init__()
+
+    def forward(self, x):
+        output = super(EmbeddingNetL2, self).forward(x)
+        output /= output.pow(2).sum(1, keepdim=True).sqrt()
+        return output
+
+    def get_embedding(self, x):
+        return self.forward(x)
+
+
+class ClassificationNet(nn.Module):
+    def __init__(self, embedding_net, n_classes):
+        super(ClassificationNet, self).__init__()
+        self.embedding_net = embedding_net
+        self.n_classes = n_classes
+        self.nonlinear = nn.PReLU()
+        self.fc1 = nn.Linear(2, n_classes)
+
+    def forward(self, x):
+        output = self.embedding_net(x)
+        output = self.nonlinear(output)
+        scores = F.log_softmax(self.fc1(output), dim=-1)
+        return scores
+
+    def get_embedding(self, x):
+        return self.nonlinear(self.embedding_net(x))
+
+
+class SiameseNet(nn.Module):
+    def __init__(self, embedding_net):
+        super(SiameseNet, self).__init__()
+        self.embedding_net = embedding_net
+
+    def forward(self, x1, x2):
+        output1 = self.embedding_net(x1)
+        output2 = self.embedding_net(x2)
+        return output1, output2
+
+    def get_embedding(self, x):
+        return self.embedding_net(x)
+
+
+class TripletNet(nn.Module):
+    def __init__(self, embedding_net):
+        super(TripletNet, self).__init__()
+        self.embedding_net = embedding_net
+
+    def forward(self, x1, x2, x3):
+        output1 = self.embedding_net(x1)
+        output2 = self.embedding_net(x2)
+        output3 = self.embedding_net(x3)
+        return output1, output2, output3
+
+    def get_embedding(self, x):
+        return self.embedding_net(x)
+
+
+class ContrastiveLoss(nn.Module):
+    """
+    Contrastive loss function.
+    Based on: http://yann.lecun.com/exdb/publis/pdf/hadsell-chopra-lecun-06.pdf
+    """
+
+    def __init__(self, margin=2.0):
+        super(ContrastiveLoss, self).__init__()
+        self.margin = margin
+
+    def forward(self, output1, output2, label):
+        euclidean_distance = F.pairwise_distance(output1, output2, keepdim = True)
+        loss_contrastive = torch.mean((1-label) * torch.pow(euclidean_distance, 2) +
+                                      (label) * torch.pow(torch.clamp(self.margin - euclidean_distance, min=0.0), 2))
+
+        return loss_contrastive
